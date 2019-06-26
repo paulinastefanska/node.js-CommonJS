90e0eae9cae284dc24b75da339b6f6a813759fc8
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-26 15:01:11 +0200

    Add simple app

---
+++ index.js
@@ -0,0 +1,23 @@
+var os = require('os');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+            case '/getOSinfo':
+                process.stdout.write('Tutaj będzie info o systemie!\n');
+                break;
+        };
+    }
+});
